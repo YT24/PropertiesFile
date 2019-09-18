3e514c9a075b81eefd1dac811e901030643d6d49
yangte66 <yangt@paraview.cn>
2019-09-18 13:48:06 +0800

    123

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,21 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-  
 
     @GetMapping("say1")
-    public String say(){
-    return "";
-    }
-  
-   public String say1(){
-    return "";
+    public String say() {
+        return "";
     }
 
-  
-   public String say2(){
-    return "";
+    public String say1() {
+        return "";
     }
 
 
+    public String say2() {
+        return "";
+    }
 }
